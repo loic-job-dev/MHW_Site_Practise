097d97345ce19f5b66c9da91d6d87d26b5fcb1fa
Loïc Jobard <loic.jobard@le-campus-numerique.fr>
2025-04-30 14:03:47 +0200

    Issue#7 subissue#1 - added gallery

---
+++ script/config.js
@@ -0,0 +1,12 @@
+const picturesFoler = [
+    "gallery_pictures/mhw-anjanath_render_001.webp",
+    "gallery_pictures/mhw-barroth_render_001.webp",
+    "gallery_pictures/mhw-great_jagras_render_001.webp",
+    "gallery_pictures/mhw-jyuratodus_render_001.webp",
+    "gallery_pictures/mhw-kulu-ya-ku_render_001.webp",
+    "gallery_pictures/mhw-pukei-pukei_render_001.webp",
+    "gallery_pictures/mhw-rathian_render_002.webp",
+    "gallery_pictures/mhw-teostra_render_001.webp",
+    "gallery_pictures/mhw-tobi-kadachi_render_001.webp",
+    "gallery_pictures/mhw-tzitzi-ya-ku_render_001.png"
+];
---
+++ script/main.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', () => {
+    //Si la page est 'index.html'
+    if(document.getElementById('index')) {
+        //Déclaration de variables globales depuis la page 'index.html'
+        const addCardButton = document.getElementById('addCardButton');
+        const addForm = document.getElementById('addForm');
+        addForm.hidden = true;
+
+        //Appel des fonctions du fichier 'script.js'
+        fetchMonsters(30).then(monsters => {
+            displayFeed(monsters);
+        });
+        
+        //Ecoute de l'événement de click sur le bouton addCardButton
+        addCardButton.addEventListener('click', () => {
+            if (!isAddDisplayed) {
+                displayForm();
+            }
+        })
+    }
+
+    if(document.getElementById('gallery')) {
+        //Déclaration de variables globales depuis la page 'gallery.html'
+        const changeDisplayPictures = document.getElementById('changeDisplayPictures');
+
+        //Appel de la fonction pour afficher les images
+        displayGallery(picturesFoler);
+
+        //Ecoute de l'événement de click sur le bouton changeDisplayPictures
+        changeDisplayPictures.addEventListener('click', function() {
+
+        })
+    }
+})
